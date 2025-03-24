1818385aa94ef3bdcc75b3dc419c3f75e324639f
Andrea Ranieri <andrea.ranieri@cnr.it>
2025-03-24 17:31:20 +0100

    Made a better register_spread.py version...

--- maskdino/data/datasets/register_spread.py
+++ maskdino/data/datasets/register_spread.py
@@ -9,8 +9,13 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets.coco import load_coco_json, register_coco_instances
 from detectron2.utils.file_io import PathManager
 
-#BASE_DIR = '/mnt/raid1/dataset'
-BASE_DIR = '/mnt/data/dataset'
+BASE_DIR = '/mnt/raid1/dataset/'
+#BASE_DIR = '/mnt/data/dataset'
+#ANNO_DIR = 'spread/coco-annotations/spread-v2-coco'
+#ANNO_DIR = 'spread/coco-annotations/spread-coco-annotations-o1-36170-samples-612023-instances-20250314'
+#ANNO_DIR = 'spread/coco-annotations/spread-coco-annotations-o1-36173-samples-961624-instances-everything-at-960x540px-20250319'
+#ANNO_DIR = 'spread/coco-annotations/spread-coco-annotations-o1-36173-samples-611442-instances-masks-480x270-polygon-annotations-20250320'
+ANNO_DIR = 'spread/coco-annotations/spread-coco-annotations-o1-36173-samples-611442-instances-masks-480x270-polygon-annotations-20250320-renamed-base-dir-to-spread-480x270'
 
 SPREAD_CATEGORIES = [{"id": 1, "name": "tree", "supercategory": "plant"},]
 
@@ -19,17 +24,6 @@ def _get_spread_instances_meta():
 	thing_classes = [k["name"] for k in SPREAD_CATEGORIES]
 
 	thing_dataset_id_to_contiguous_id = {k: i for i, k in enumerate(thing_ids)}
-	'''
-	assert len(thing_ids) == 100, len(thing_ids)
-	# Mapping from the incontiguous ADE category id to an id in [0, 99]
-	thing_dataset_id_to_contiguous_id = {k: i for i, k in enumerate(thing_ids)}
-	thing_classes = [k["name"] for k in SPREAD_CATEGORIES]
-	ret = {
-		"thing_dataset_id_to_contiguous_id": thing_dataset_id_to_contiguous_id,
-		"thing_classes": thing_classes,
-	}
-	return ret
-	'''
 	ret = {
 		"thing_dataset_id_to_contiguous_id"	: thing_dataset_id_to_contiguous_id,
 		"thing_classes"				: thing_classes,
@@ -38,39 +32,13 @@ def _get_spread_instances_meta():
 	return ret
 
 def register_all_spread(spread_dir):
-	register_coco_instances("spread_train", _get_spread_instances_meta(), f"{spread_dir}/train.json", BASE_DIR + "/spread/spread-v2-coco")
-	register_coco_instances("spread_valid", _get_spread_instances_meta(), f"{spread_dir}/valid.json", BASE_DIR + "/spread/spread-v2-coco")
-	register_coco_instances("spread_test" , _get_spread_instances_meta(), f"{spread_dir}/test.json" , BASE_DIR + "/spread/spread-v2-coco")
+	register_coco_instances("spread_train", _get_spread_instances_meta(), f"{spread_dir}/train.json", BASE_DIR + ANNO_DIR)
+	register_coco_instances("spread_valid", _get_spread_instances_meta(), f"{spread_dir}/valid.json", BASE_DIR + ANNO_DIR)
+	register_coco_instances("spread_test" , _get_spread_instances_meta(), f"{spread_dir}/test.json" , BASE_DIR + ANNO_DIR)
 
-'''
-def _get_ade_instances_meta():
-    thing_ids = [k["id"] for k in ADE_CATEGORIES]
-    assert len(thing_ids) == 100, len(thing_ids)
-    # Mapping from the incontiguous ADE category id to an id in [0, 99]
-    thing_dataset_id_to_contiguous_id = {k: i for i, k in enumerate(thing_ids)}
-    thing_classes = [k["name"] for k in ADE_CATEGORIES]
-    ret = {
-        "thing_dataset_id_to_contiguous_id": thing_dataset_id_to_contiguous_id,
-        "thing_classes": thing_classes,
-    }
-    return ret
-
-
-def register_all_ade20k_instance(root):
-    for key, (image_root, json_file) in _PREDEFINED_SPLITS.items():
-        # Assume pre-defined datasets live in `./datasets`.
-        register_coco_instances(
-            key,
-            _get_ade_instances_meta(),
-            os.path.join(root, json_file) if "://" not in json_file else json_file,
-            os.path.join(root, image_root),
-        )
-'''
-
-
-
-#SPREAD_DIR = '/mnt/raid1/dataset/spread/spread-v2'
-#SPREAD_DIR = '/mnt/data/dataset/spread/spread-v2'
-SPREAD_DIR = BASE_DIR + '/spread/spread-v2'
+#SPREAD_ANNO_DIR = '/mnt/raid1/dataset/spread/spread-v2'
+#SPREAD_ANNO_DIR = '/mnt/data/dataset/spread/spread-v2'
+#SPREAD_ANNO_DIR = BASE_DIR + '/spread/spread-v2'
+SPREAD_ANNO_DIR = BASE_DIR + ANNO_DIR
 #_root = os.getenv("DETECTRON2_DATASETS", "datasets")
-register_all_spread(SPREAD_DIR)
+register_all_spread(SPREAD_ANNO_DIR)
